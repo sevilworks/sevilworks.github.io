c8e912091263421c7df626965cdf595e6aff6b81
sevilworks <sevilme100@gmail.com>
2025-09-28 16:29:57 -0500

    modifieb vite

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    minify: 'esbuild'
-  }
 })
