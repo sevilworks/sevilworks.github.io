95a777126033936db99cb04d81b58f5fa3be5e04
sevilworks <sevilme100@gmail.com>
2025-09-28 16:35:04 -0500

    base path set tp slash

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/sevilworks.github.io/', // Set base to match your repo name for GitHub Pages
+  base: '/', // For user/organization pages, use '/'
 })
